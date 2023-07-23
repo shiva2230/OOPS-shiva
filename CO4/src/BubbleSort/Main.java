a5abeb16948408875b239ddc32b1f1b2d23b555a
Shiva <2230@tkmce.ac.in>
2023-07-23 17:11:09 +0530

    updated files

--- CO4/src/BubbleSort/Main.java
+++ CO4/src/BubbleSort/Main.java
@@ -3,29 +3,47 @@ package BubbleSort;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        int temp;
-        ArrayList<Integer> list=new ArrayList<>();
-        list.add(1);
-        list.add(56);
-        list.add(7);
-        list.add(18);
-        list.add(23);
-        list.add(98);
-        System.out.println("unsorted list: "+list);
-        for (int i=0;i<list.size()-1;i++){
-            for (int j=i+1;j<(list.size()-i-1);j++){
-                int num1=list.get(i);
-                int num2=list.get(j);
-                if (num1>num2){
-                    temp=num2;
-                    num1=num2;
-                    num2=temp;
+
+    public static <T extends Comparable<T>> void bubbleSort(ArrayList<T> list) {
+        int n = list.size();
+        T temp;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                T num1 = list.get(j);
+                T num2 = list.get(j + 1);
+                if (num1.compareTo(num2) > 0) {
+                    temp = num1;
+                    list.set(j, num2);
+                    list.set(j + 1, temp);
                 }
             }
         }
-        System.out.println("sorted list: "+list);
+    }
 
+    public static void main(String[] args) {
+        ArrayList<Integer> intList = new ArrayList<>();
+        intList.add(1);
+        intList.add(56);
+        intList.add(7);
+        intList.add(18);
+        intList.add(23);
+        intList.add(98);
+
+        System.out.println("Unsorted list: " + intList);
+        bubbleSort(intList);
+        System.out.println("Sorted list: " + intList);
+
+        ArrayList<String> strList = new ArrayList<>();
+        strList.add("apple");
+        strList.add("orange");
+        strList.add("banana");
+        strList.add("pear");
+        strList.add("grape");
+
+        System.out.println("Unsorted list: " + strList);
+        bubbleSort(strList);
+        System.out.println("Sorted list: " + strList);
     }
 
 }
